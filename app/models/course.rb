eab4bb2e01c95147833bfcbf07b6090d4b3dc790
Kazama <narusai97@gmail.com>
2016-06-26 16:38:31 +0900

    ほぼ完成

--- app/models/course.rb
+++ app/models/course.rb
@@ -1,2 +1,4 @@
 class Course < ActiveRecord::Base
+  has_many :participants
+  validates :title, presence: true
 end
--- app/models/participant.rb
+++ app/models/participant.rb
@@ -1,2 +1,19 @@
 class Participant < ActiveRecord::Base
+  belongs_to :course, dependent: :destroy
+  validates :name, presence: true
+  validate :upper_limit?
+
+  private
+  def upper_limit?
+    # 検証対象のコースを取得する
+    course = Course.find(course_id)
+    # 現在の参加者を調べる
+    participants_count = course.participants.count
+
+    # 現在の参加者数とコースの上限を比較する
+    if participants_count >= course.upper_limit
+      # 上限に達していればエラーログを出して保存させない
+      errors.add(:base, "上限に達しました")
+    end
+  end
 end
